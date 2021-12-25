51da1b05d83b14f1361bed33ffb5b7188652d24e
Amr Elsayed <amrmostafa1995@gmail.com>
2021-12-25 11:03:59 +0200

    Time: 1 ms (99.62%), Space: 37.4 MB (70.80%) - LeetHub

---
+++ detect-capital/detect-capital.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        
+        boolean flag=true;
+        for(Character c : word.toCharArray())
+            if(!Character.isUpperCase(c))
+              flag=  false;
+        if(flag)
+            return true;
+                
+        
+        flag=true;
+        for(Character c : word.toCharArray())
+            if(Character.isUpperCase(c))
+                flag= false;
+        if(flag)
+            return true;
+                
+        
+        flag=true;
+        if(!Character.isUpperCase(word.charAt(0)))
+            flag=false;
+        for(int i=1;i<word.length();i++)
+            if(Character.isUpperCase(word.charAt(i)))
+               flag= false;
+     return flag;         
+    }
+}
