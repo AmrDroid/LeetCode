13e89b146c1b339b375ee957e2c7d0b6dfb4c192
Amr Elsayed <amrmostafa1995@gmail.com>
2022-01-17 11:19:40 +0200

    Time: 26 ms (39.55%), Space: 119.2 MB (28.56%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -10,10 +10,12 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
           
             sum += nums[i];
+            
             if (k != 0)
                 sum = sum % k;
             
             if (map.containsKey(sum)) {
+            
                 if (i - map.get(sum) > 1)
                     return true;
             } else
