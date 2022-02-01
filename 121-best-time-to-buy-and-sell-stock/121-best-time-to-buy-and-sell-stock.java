bf2513096004ab905eec7557d170e3f2a4f95e7c
Amr Elsayed <amrmostafa1995@gmail.com>
2022-02-01 10:20:22 +0200

    Time: 4 ms (30.75%), Space: 83.4 MB (55.13%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -17,5 +17,32 @@ class Solution {
         }
         return res;
         
+        
+        
+        /*
+        
+          
+         if (prices == null || prices.length < 2) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int minPrice = prices[0]; // the minimum price from the first day to day k-1
+        int[] first = new int[prices.length];
+        // the maximum profit with at most one transaction from the first day to day k
+        for (int i = 1; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            first[i] = maxProfit;
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+       
+        maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, first[i] );
+        }
+        return maxProfit;
+        
+        
+        */
+        
     }
 }
