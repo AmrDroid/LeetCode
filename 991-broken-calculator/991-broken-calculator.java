007d3c3df3cd1f325fc96a134b3f6303bebd7464
Amr Elsayed <amrmostafa1995@gmail.com>
2022-03-24 12:38:31 +0200

    Time: 0 ms (100.00%), Space: 39.4 MB (74.31%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.java
+++ 991-broken-calculator/991-broken-calculator.java
@@ -1,11 +1,25 @@
 class Solution {
     public int brokenCalc(int X, int Y) {
-         if (X >= Y){
+         int count = 0;
+        while (Y > X){
+            count++;
+            if (Y % 2 == 1){
+                Y++;
+            }else{
+                Y /= 2;
+            }
+        }
+        return count + X - Y;
+    
+        
+        /*  if (X >= Y){
             return X - Y;
         }
         if (Y % 2 == 1){
             return brokenCalc(X, Y + 1) + 1;
         }
         return brokenCalc(X, Y/2) + 1;
+    
+    */
     }
 }
