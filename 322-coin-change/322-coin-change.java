c4902edda8f98ae01645bd7a7ef360ca856c042b
Amr Elsayed <amrmostafa1995@gmail.com>
2022-04-14 14:32:06 +0200

    Time: 64 ms (16.39%), Space: 42.4 MB (84.57%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,26 @@
+class Solution {
+int ans=Integer.MAX_VALUE;
+    
+    public int coinChange(int[] coins, int amount) {
+         int[]amounts = new int[amount+1];
+        Arrays.fill(amounts,amount+1);
+        Arrays.sort(coins);
+        int len = coins.length;
+        amounts[0]=0;
+        for(int i=1;i<=amount;i++)
+        {
+        
+         for(int coin : coins)
+         {
+             if(i-coin>=0)
+                 amounts[i]=Math.min(amounts[i],amounts[i-coin]+1);             
+     
+         }            
+        }
+        System.out.println(Arrays.toString(amounts));
+     
+        
+        return amounts[amount]==amount+1? -1:amounts[amount];
+        
+    }
+}
