f971b6b48bf79637ca27dbe219b607e91685635d
Amr Elsayed <amrmostafa1995@gmail.com>
2022-04-15 14:34:00 +0200

    Time: 0 ms (100.00%), Space: 39.8 MB (83.49%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+       int prev = 0, 
+        cur = 0, 
+        ans = 0;
+        
+    for (int i = 0; i < nums.length; i++) {
+       
+        ans = Math.max(cur, prev + nums[i]);
+        
+        prev = cur;
+        
+        cur = ans;
+    
+    }
+    
+        return ans;
+    }
+    
+}
