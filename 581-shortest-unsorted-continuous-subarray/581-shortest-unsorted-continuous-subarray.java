9d61362a2170246e527dae990e3b98a0a0b04020
Amr Elsayed <amrmostafa1995@gmail.com>
2022-04-16 21:39:49 +0200

    Time: 1 ms (98.54%), Space: 42.9 MB (88.91%) - LeetHub

---
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findUnsortedSubarray(int[] array) {
+        
+         // Write your code here.
+    int len = array.length;
+
+        
+        if(len<=1)return 0;
+        
+        int maxUnsorted = Integer.MIN_VALUE, minUnsorted = Integer.MAX_VALUE;
+
+    for (int i = 0; i < len; i++) {
+      if (!isIndexUnsorted(i, array)) continue;
+
+      minUnsorted = Math.min(minUnsorted, array[i]);
+      maxUnsorted = Math.max(maxUnsorted, array[i]);
+    }
+
+    if (minUnsorted == Integer.MAX_VALUE) return 0;
+
+    int firstIdx = 0;
+    while (array[firstIdx] <= minUnsorted) ++firstIdx;
+
+    int secondIdx = array.length - 1;
+    while (array[secondIdx] >= maxUnsorted) --secondIdx;
+
+    return secondIdx-firstIdx+1;
+        //new int[] {firstIdx, };
+    }
+    
+     private static boolean isIndexUnsorted(int index, int[] array) {
+    if (index == 0) return array[index] > array[index + 1];
+
+    if (index == array.length - 1) return array[index] < array[index - 1];
+
+    return array[index] < array[index - 1] || array[index] > array[index + 1];
+  }
+
+}
