cf3b727e3e555d65baa9f894694ae19a27b79bff
Amr Elsayed <amrmostafa1995@gmail.com>
2022-04-21 22:39:07 +0200

    Time: 3 ms (87.55%), Space: 42 MB (98.47%) - LeetHub

---
+++ 51-n-queens/51-n-queens.java
@@ -0,0 +1,79 @@
+class Solution {
+  
+    List<List<String>> res = new ArrayList<>();
+    
+    public List<List<String>> solveNQueens(int n) {
+        //initialize the board
+    
+        char[][] board = new char[n][n];
+        
+        for (char[] c : board) {
+            Arrays.fill(c, '.');
+        }
+        
+        
+        helper(0, board);
+        
+        return res;
+    }
+    
+    private void helper(int row, char[][] board) {
+      
+        int n = board.length;
+        
+        //base case, if found, then return        
+        if (row == board.length) {
+            res.add(toList(board));
+            return;
+        }
+        
+        
+        //core
+        for (int j = 0; j < n; j++) {
+           
+            if (!isValid(board, row, j)) {
+                continue;
+            }
+            
+            board[row][j] = 'Q';
+            
+            helper(row + 1, board);
+            
+            board[row][j] = '.';
+        }
+        
+        
+    }
+    private boolean isValid(char[][] board, int row, int col) {
+        int n = board.length;
+        
+        //each col only contains one queen
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') 
+                return false;
+        }
+        
+        //left up no queen
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') 
+                return false;
+        }
+        
+        //right up no queen
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') 
+                return false;
+        }
+        
+        return true;
+    }
+    
+    private List toList(char[][] board) {
+        List<String> list = new ArrayList<>();
+        for (char[] c : board) {
+            list.add(String.valueOf(c));
+        }
+        return list;
+    }
+    
+}
