05b6cd3bf14608b05cf619c35dc9b116f005f0e1
Amr Elsayed <amrmostafa1995@gmail.com>
2022-04-23 21:55:58 +0200

    Time: 11 ms (64.61%), Space: 53.9 MB (58.43%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,40 +1,53 @@
 class Solution {
-    int maxLen= 0;//Stores the Maximum Increasing path length in the matrix 
-    int[][] dp;
-    
     public int longestIncreasingPath(int[][] matrix) {
+    
+        int n= matrix.length;
+        int m=matrix[0].length;
         
-        dp= new int[matrix.length][matrix[0].length];//Each cell in dp store's the maximum increasing path from that node 
+        int dp[][]=new int[n][m];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                dp[i][j]=-1;
         
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if ( dp[i][j] == 0){//Only exploring the non explored node 
-                    maxLen= Math.max(maxLen, dfs(i, j, -1, matrix));    
+    
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if (dp[i][j] == -1) {
+                    dp[i][j] = LIP(dp, matrix, n, m, i, j);
+
                 }
             }
-        }
-        return maxLen;
-    }
+
+         int max = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                max = Math.max(max, dp[i][j]);
+            }
+
+        return max;    
+        
     
-    private int dfs(int i, int j, int prev, int[][] matrix) {//prev to store the previous value visited 
-        
-        if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || matrix[i][j] <= prev) //Base Case 
-            return 0;
-        
-        
-        if (dp[i][j] > 0) //If incresing path exists from this coordinate, we are returning it from dp table 
-            return dp[i][j];
-        
-        int res= 0;//stores the maximum incresing path length for this co-ordinate 
-        
-        //+1 because to include the current node 
-        res= Math.max(res, 1 + dfs(i + 1, j, matrix[i][j], matrix));//down 
-        res= Math.max(res, 1 + dfs(i - 1, j, matrix[i][j], matrix));//up
-        res= Math.max(res, 1 + dfs(i, j + 1, matrix[i][j], matrix));//left
-        res= Math.max(res, 1 + dfs(i, j - 1, matrix[i][j], matrix));//right
-        
-        dp[i][j]= res;//memorising the increasing path length
-        
-        return res;//returning to parent to decide their maximum path length
+    }
+  int LIP(int dp[][], int mat[][], int n, int m, int x, int y) {
+        // If value not calculated yet.
+        if (dp[x][y] < 0) {
+            int result = 0;
+
+            if (x - 1 >= 0 && mat[x][y] < mat[x - 1][y])
+                result = Math.max(result,1 + LIP(dp, mat, n, m, x - 1, y));
+
+            if (x + 1 < n && mat[x][y] < mat[x + 1][y])
+                result =Math.max(result, 1 + LIP(dp, mat, n, m, x + 1, y));
+
+            if (y + 1 < m && mat[x][y] < mat[x][y + 1])
+                result =  Math.max(result,1 + LIP(dp, mat, n, m, x, y + 1));
+
+            if (y - 1 >= 0 && mat[x][y] < mat[x][y - 1])
+                result =Math.max(result,1 + LIP(dp, mat, n, m, x, y - 1));
+
+            dp[x][y] = Math.max(result, 1);
+        }
+
+        return dp[x][y];
     }
 }
