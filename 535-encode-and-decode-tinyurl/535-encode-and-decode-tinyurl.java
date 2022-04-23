7e25ee5e01b252b4b37a9c26f7aca388c0ddb689
Amr Elsayed <amrmostafa1995@gmail.com>
2022-04-23 11:20:32 +0200

    Time: 9 ms (29.65%), Space: 43.7 MB (43.38%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -0,0 +1,43 @@
+public class Codec {
+
+
+    //encode with the entire 62 strings instead of just integer
+    String alphabet = 
+        "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    HashMap<String, String> map = new HashMap<>();
+    
+    //assign random
+    Random rand = new Random();
+    
+    //Initalise to get a random key
+    String key = getRand();
+    
+    public String getRand() {
+        //creates a mutable sequence of characters
+        StringBuilder sb = new StringBuilder();
+        //the shortened URL within the length of 6 
+        for (int i = 0; i < 6; i++) {
+            sb.append(alphabet.charAt(rand.nextInt(62)));
+        }
+        return sb.toString();
+    }
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        while (map.containsKey(key)) {
+            key = getRand();
+        }
+        map.put(key, longUrl);
+        return "http://tinyurl.com/" + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl.replace("http://tinyurl.com/", ""));
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
