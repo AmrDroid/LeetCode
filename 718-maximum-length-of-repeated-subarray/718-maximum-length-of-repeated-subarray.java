88ecc1a03d89fde93c8f7a6036da2f2e946402ec
Amr Elsayed <amrmostafa1995@gmail.com>
2022-09-20 13:27:12 +0200

    Time: 97 ms (33.13%), Space: 77.3 MB (69.77%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findLength(int[] A, int[] B) {
+     
+        int ans = 0;
+        int[][] dp = new int[A.length+1][B.length+1];
+        for(int i = 1; i <= A.length; i++)
+        {
+            for(int j = 1; j <= B.length; j++)
+            {
+                if(A[i-1] == B[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1]; //dp[i-1][j-1] is basically the max length of repeated subarray without the latest elements added from A and B
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans;   
+    }
+}
