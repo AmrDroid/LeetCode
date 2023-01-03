405d9182960ff8bb8db9dc52a0b1d42de8e0d427
Amr Elsayed <amrmostafa1995@gmail.com>
2023-01-03 10:28:54 -0600

    Time: 9 ms (84.49%), Space: 42.3 MB (85.84%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,45 +1,62 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
         
-        int n= strs.length;
-        int len = strs[0].length();
-        char arr[][]=new char[n][len];
-        
-        for(int i=0;i<n;i++)
+            
+        int remove = 0;
+        for(int i=0;i<strs[0].length();i++)
         {
-            for(int j=0;j<len;j++)
+            int max = 0;
+            for(int j=0;j<strs.length;j++)
             {
-                arr[i][j]=strs[i].charAt(j);
-            }
-        }
-        
-        int res=0;
-        
-        for(char a[]:arr)
-                    System.out.println(Arrays.toString(a));
-        
-        // System.out.println(arr[0].length);
-        
-        for(int i=0;i<arr[0].length;i++)
-        {
-
-            char prev=arr[0][i];
-            for(int j=1;j<arr.length;j++)
-            {
-                if(arr[j][i]<prev)
+                if(max==0)
                 {
-                    res++;
-                    //j++;
-                break;  
+                    max = strs[j].charAt(i)-0;
                 }
-                prev=arr[j][i];        
+                if (strs[j].charAt(i)-0<max)
+                {
+                    remove+=1;
+                    break;
+                }
+                else{
+                    max = strs[j].charAt(i)-0;
+                }
             }
         }
+        return remove;
+        
+//         int n= strs.length;
+//         int len = strs[0].length();
+//         char arr[][]=new char[n][len];
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=0;j<len;j++)
+//             {
+//                 arr[i][j]=strs[i].charAt(j);
+//             }
+//         }
+        
+//         int res=0;
+        
+//         for(int i=0;i<arr[0].length;i++)
+//         {
+
+//             char prev=arr[0][i];
+//             for(int j=1;j<arr.length;j++)
+//             {
+//                 if(arr[j][i]<prev)
+//                 {
+//                     res++;
+//                   break;  
+//                 }
+//                 prev=arr[j][i];        
+//             }
+//         }
         
         
         
         
         
-        return res;
+//         return res;
     }
 }
