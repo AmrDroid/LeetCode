be8a34b61bc10cc5fe94700f87bf6ab2791d8373
Amr Elsayed <amrmostafa1995@gmail.com>
2023-04-03 15:22:12 -0500

    Time: 17 ms (98.32%), Space: 50.9 MB (39.23%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+      int res = 0, n = people.length, 
+        left = 0,
+        right = n - 1;
+        Arrays.sort(people);
+            
+        while (left <= right) {
+        	if (people[left] + people[right] <= limit) 
+                ++left;
+            
+            
+        	--right;
+        	++res;
+        }
+        return res;
+    }
+}
+
+//1 2 2 3
