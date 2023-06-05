bef07b38ab34f6a9ca66bcad80f45536e8ac704d
Amr Elsayed <amrmostafa1995@gmail.com>
2023-06-05 16:20:43 -0500

    Time: 0 ms (100.00%), Space: 42.8 MB (25.73%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+       
+  
+     int x1=coordinates[0][0];
+     int y1=coordinates[0][1];
+     int x2=coordinates[1][0];
+     int y2=coordinates[1][1];
+
+     //eq  c =ax+by;
+      int a=y2-y1;
+      int b=x1-x2;
+      int c=(a*x1)+(b*y1);
+
+      for(int i=2;i<coordinates.length;i++){
+        int c1=a*coordinates[i][0]+b*coordinates[i][1];
+        if(c1!=c) return false;
+      }
+      return true;
+    }
+}
